a45eeb47c9169ee7fb0cf3e0a87049aa3560638d
geoyee <geoyee@yeah.net>
2022-02-20 18:30:21 +0800

    Update add reader

---
+++ gpsexif/reader.py
@@ -0,0 +1,35 @@
+import piexif
+import numpy as np
+from PIL import Image
+
+
+def getGPSfromImage(img_path):
+    img = Image.open(img_path)
+    if isinstance(img.info, dict):
+        exif_dict = piexif.load(img.info["exif"])
+        gps = exif_dict["GPS"]
+        lon = __dict2Loc(gps[2]) * (1 if gps[1] == b"N" else -1)
+        lat = __dict2Loc(gps[4]) * (1 if gps[3] == b"E" else -1)
+        return np.array([lat, lon])
+    else:
+        raise EXIFError("This photo has not EXIF!")
+
+
+class EXIFError(Exception):
+    def __init__(self, str):
+        self.str = str
+    def __str__(self):
+        print(self.str)
+
+
+def __dict2Loc(dct):
+    D = dct[0][0] / dct[0][1]
+    F = dct[1][0] / dct[1][1]
+    M = dct[2][0] / dct[2][1]
+    loc = D + F / 60 + M / 3600
+    return loc
+
+
+if __name__ == "__main__":
+    img_path = "testdata/whu.jpg"
+    print(getGPSfromImage(img_path))
---
+++ gpsexif/writer.py
@@ -0,0 +1,3 @@
+import piexif
+from PIL import Image
+
