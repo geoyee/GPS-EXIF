63e57ed9029e2b634915e1f510256bfeaafae260
geoyee <geoyee@yeah.net>
2022-02-21 12:31:39 +0800

    Test python-publish

--- setup.py
+++ setup.py
@@ -2,19 +2,22 @@ import setuptools
 
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    LONG_DESCRIPTION = fh.read()
+    
+with open("requirements.txt") as fin:
+    REQUIRED_PACKAGES = fin.read()
 
 setuptools.setup(
     name="gpsexif",
-    version="0.1",
+    version="0.2",
     author="geoyee",
     author_email="geoyee@yeah.net",
     description="Writing / Reading GPS coordinates in photo's EXIF by Python.",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/geoyee/GPS-EXIF",
     packages=setuptools.find_packages(),
-    install_requires=["piexif"],
+    install_requires=REQUIRED_PACKAGES,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
